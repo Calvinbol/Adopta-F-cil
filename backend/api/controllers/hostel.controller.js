67523ef6ec4effdb63fd90f2858353f2b022a13b
Calvinbol <mayrelis02@hotmail.com>
2024-06-18 16:48:20 +0100

    Peticiones Hostel

--- backend/api/controllers/hostel.controller.js
+++ backend/api/controllers/hostel.controller.js
@@ -1,4 +1,4 @@
-const db = require('../models');
+
 const bcrypt = require('bcrypt');
 const Hostel = require('../models/hostel.model');
 
--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const authRoutes = require('./auth.routes');
 const petRoutes = require('./pet.route.js');
+const hostelRoutes = require ('./hostel.route.js')
 
 
 
 router.use('/auth', authRoutes);
 router.use('/pet', petRoutes);
+router.use('/hostel', hostelRoutes)
 
 module.exports = router;
