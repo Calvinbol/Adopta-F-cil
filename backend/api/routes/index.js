42d60508ea12657f1794b9df7eb65cb5e7b0882c
Calvinbol <mayrelis02@hotmail.com>
2024-06-18 10:00:18 +0100

    socorro

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router()
 
 
 router.use('/auth', require('./auth.route')) //Functions of Signup and Login for all users
-router.use('/user', require('./user.route'))
 
 
 module.exports = router
--- backend/api/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const { getUsers } = require('../controllers/user.controller')
-const { checkAuth } = require('../middlewares/auth.middlewares')
-
-const router = require('express').Router()
-
-router.get('/', checkAuth,  getUsers)
-
-module.exports = router
