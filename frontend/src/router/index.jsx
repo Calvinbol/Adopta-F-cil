01658189a5fc8da222f8796c20a4d9825623da0a
Calvinbol <mayrelis02@hotmail.com>
2024-06-19 16:09:43 +0100

    Corregido

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, redirect } from 'react-router-dom'
 import App from '../App'
 import Layout from '../Layouts/Layout'
 import Signup from '../pages/Signup/Signup'
-import LoginForm from '../componentes/LoginFrom'
+import LoginForm from '../components/LoginFrom'
 
 
 export const router = createBrowserRouter([
