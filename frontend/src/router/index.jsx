56f80a90e604a8715e3b541a65a0e84ce87fe140
sstefanyof <stefany-of@hotmail.com>
2024-06-20 12:25:13 +0100

    ReOrganizacion

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,28 +1,47 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
+import Home from '../components/Home'
 import Layout from '../Layouts/Layout'
 import Signup from '../pages/Signup/Signup'
 import LoginForm from '../components/LoginFrom'
+import Mascotas from '../components/Mascotas'
+import Contacto from '../components/Contacto'
+import Adoptar from '../components/Adoptar'
 
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout />,
-        children: [
-            {
-                path: '/',
-                element: <App />,
-            },
-            {
-                path: '/signup',
-                element: <Signup />,
-            },
-            {
-                path: '/login',
-                element: <LoginForm />,
-            },
-            
-        ],
-    },
-])
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/login",
+        element: <LoginForm />,
+      },
+      {
+        path: "/mascotas",
+        element: <Mascotas />,
+      },
+      {
+        path: "/contacto",
+        element: <Contacto />,
+      },
+      {
+        path: "/adoptar",
+        element: <Adoptar/>
+      },
+      {
+        path: "/login",
+        element: ''
+      }
+    ],
+  },
+]);
