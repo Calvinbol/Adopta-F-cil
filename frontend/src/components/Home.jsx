691188aade83c0103ebfdb23516c72de80a5f0f5
sstefanyof <stefany-of@hotmail.com>
2024-06-20 16:37:40 +0100

    home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css"; 
 
 const Home = () => {
-    return (
-            <div className="home-background">
+  const handleClick = () => {
+    
+    console.log("Botón clickeado");
+    
+  };
 
-            </div>
-    );
+  return (
+    <div className="home-background">
+      <div className="text">
+        <a className="greyFont">¡Dale una </a>
+        <a className="blueBold">nueva </a>
+        <p className="greyFont">oportunidad a un </p>
+        <a className="blueBold">Perro o Gato y</a>
+        <p className="greyFont">transforma tu vida!</p>
+        <p className="blueBold"></p>
+
+        {}
+        <button className="button" onClick={handleClick}>
+          Conoce a tu mejor amigo
+        </button>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
 };
 
 export default Home;
